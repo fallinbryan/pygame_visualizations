ab9731b98e39a15442a1f575dfa48440893c4a3b
bfallin <fallinbryan@gmail.com>
2018-05-29 15:40:52 -0500

    evol creatures move around, eat, and mate.  mates are clones right now. need to add the genes and cross function

--- evol_eco.py
+++ evol_eco.py
@@ -9,6 +9,14 @@ from Physics import Physics
 
 random.seed()
 
+
+class Target:
+    def __init__(self):
+        self.position = Vector2D(0, 0)
+
+    def move(self, arg):
+        self.position = Vector2D(arg[0], arg[1])
+
 class Compass(object):
     def __init__(self, surface, pos, title=None):
         self.surface = surface
@@ -71,18 +79,30 @@ class Creature(Physics):
         self.max_speed = 20
         self.prev_acc = Vector2D(0, 0)
         self.state = "wandering"
+        self.target = None
+        self.targetReached = True
 
     def apply_force(self, force):
         super().apply_force(force)
         self.prev_acc = force
 
-    def seek(self, target):
+    def seek(self):
+        target = self.target
         desired = target.position.sub(self.position)
         desired = desired.normalize()
         desired = desired.mul(self.position.distance(target.position))
         force = desired.sub(self.velocity)
+        self.apply_force(force)
+        mag = self.position.distance(target.position)
+        if mag < 20:
+            self.targetReached = True
+            mag = 0
+        self.velocity.set_magnitude(mag * 0.5)
+        if self.velocity.magnitude() > self.max_speed:
+            self.velocity.set_magnitude(self.max_speed)
 
-        return force
+        super().update()
+
 
     def test_seek(self, target):
         force = self.seek(target)
@@ -98,16 +118,18 @@ class Creature(Physics):
         min_dist = 9999999
         target = targets[0]
         for _target in targets:
-            if self.position.distance(target.position) < min_dist:
+            if self.position.distance(_target.position) < min_dist:
                 if _target is not self:
-                    min_dist = self.position.distance(target.position)
+                    min_dist = self.position.distance(_target.position)
                     target = _target
-        force = self.seek(target)
+        self.target = target
+        self.seek()
         found = None
-        if self.position.distance(target.position) < 50:
+
+        if self.position.distance(self.target.position) < 20:
             found = target
 
-        return force, found
+        return found
 
     def eat_food(self, food):
         self.energy += food.energy
@@ -121,24 +143,15 @@ class Creature(Physics):
 
         x_max = self.surface.get_width()
         y_max = self.surface.get_height()
-        class target:
-            position = Vector2D(x_max/2, y_max/2)
-        force = self.velocity.random_vector(math.pi)
-        force.set_magnitude(0.01)
-        if int(self.position.x) in range(0, 100):
-            force = self.seek(target)
-        elif int(self.position.x) in range (x_max - 100, x_max):
-            force = self.seek(target)
-        if int(self.position.y) in range(0, 500):
-            force = self.seek(target)
-        elif int(self.position.y) in range(y_max - 100, y_max):
-            force = self.seek(target)
-
-        return force
-
-
+        target = Target()
+        target.move((random.randrange(0, x_max), random.randrange(0, y_max)))
+        if self.targetReached:
+            self.target = target
+            self.targetReached = False
+        self.seek()
 
     def C_update(self, environment):
+        return_object = None
         if self.energy == 0:
             self.alive = False
         if self.age > 5000.0:
@@ -146,29 +159,26 @@ class Creature(Physics):
 
         if self.alive:
             self.energy -= 2
-            self.age += 0.5
+            self.age += 1
 
-            if self.energy < 100:
+            if self.energy < 500:
                 self.state = "seeking food"
-                force, found = self.seek_nearest(environment['food'])
+                found = self.seek_nearest(environment['food'])
                 if found:
                     self.eat_food(found)
 
-            elif self.age > 500 and self.energy > 100:
+            elif self.age > 50 and self.energy > 500:
                 self.state = "seeking mate"
-                force, found = self.seek_nearest(environment['creatures'])
+                found = self.seek_nearest(environment['creatures'])
                 if found:
-                    self.breed(found.dna)
+                    return_object = self.breed(found.dna)
                     self.alive = False
             else:
                 self.state = 'wandering'
-                force = self.wander()
+                self.wander()
 
 
-            self.apply_force(force)
-            super().update()
-            if self.velocity.magnitude() > self.max_speed:
-                self.velocity.set_magnitude(self.max_speed)
+        return return_object
 
     def draw(self):
         color = self.color_map[self.state]
@@ -182,10 +192,13 @@ class Creature(Physics):
         pointlist.append((int(self.position.x - 15*math.cos(self.velocity.getTheta())), int(self.position.y + 15*math.sin(self.velocity.getTheta()))))
         pg.draw.polygon(self.surface, color, pointlist, 1)
 
+        # if self.target:
+        #     pg.draw.line(self.surface, color, self.position(), self.target.position(), 1)
+
 class Food(object):
     def __init__(self, x, y):
         self.position = Vector2D(x, y)
-        self.energy = 25
+        self.energy = 100
         self.eaten = False
 
 if __name__ == '__main__':
@@ -206,9 +219,9 @@ if __name__ == '__main__':
     WIDTH = canvas.get_width()
     HEIGHT = canvas.get_height()
 
-    # environment = {"food": [Food(random.randrange(0, WIDTH), random.randrange(0, HEIGHT)) for _ in range(100)],
-    #                "creatures": [Creature(canvas, Vector2D(random.randrange(0, WIDTH), random.randrange(0, HEIGHT)), DNA()) for _ in range(10)]}
-
+    environment = {"food": [Food(random.randrange(0, WIDTH), random.randrange(0, HEIGHT)) for _ in range(100)],
+                   "creatures": [Creature(canvas, Vector2D(random.randrange(0, WIDTH), random.randrange(0, HEIGHT)), DNA()) for _ in range(10)]}
+    #
     # environment = {"food": [Food(WIDTH/2, HEIGHT/2) for _ in range(1)],
     #                "creatures": [
     #                    Creature(canvas, Vector2D(random.randrange(0, WIDTH), random.randrange(0, HEIGHT)), DNA()) for _
@@ -223,7 +236,7 @@ if __name__ == '__main__':
     creature = Creature(canvas, Vector2D(WIDTH/2, HEIGHT/2), DNA())
     compass = Compass(canvas, Vector2D(WIDTH*0.1, HEIGHT*0.1), 'Creature Heading')
     isGrabbed = False
-    while  True:
+    while True:
         # pg.time.wait(100)
         for event in pg.event.get():
             if event.type == pg.QUIT:
@@ -249,33 +262,38 @@ if __name__ == '__main__':
                     sys.exit(1)
 
         canvas.fill(disp_bg_color)
-        # if environment["food"]:
-        #     for food in environment["food"]:
-        #         if food.eaten:
-        #             environment["food"].pop(environment["food"].index(food))
-        #         if not food.eaten:
-        #             pg.draw.circle(canvas, (50, 255, 50), food.position.floor()(), 2, 0)
+        if environment["food"]:
+            for food in environment["food"]:
+                if food.eaten:
+                    environment["food"].pop(environment["food"].index(food))
+                if not food.eaten:
+                    pg.draw.circle(canvas, (50, 255, 50), food.position.floor()(), 2, 0)
+
+            for creature in environment["creatures"]:
+                if creature.alive:
+                    new_creature = creature.C_update(environment)
+                    creature.draw();
+                    if new_creature:
+                        environment["creatures"].append(new_creature)
+                else:
+                    environment['food'].append(Food(creature.position.x, creature.position.y))
+                    environment['creatures'].pop(environment['creatures'].index(creature))
+
+        # target.move(pg.mouse.get_pos())
+        # pg.draw.circle(canvas, (255, 255, 255), (target.position.x, target.position.y), 20, 1)
+        # creature.test_seek(target)
+        # creature.update()
+        # creature.draw()
+        # compass.show(creature.velocity.getTheta())
+        # heading_text = font.render('Heading: {:.02f}  Velocity: {:.02f}'.format(creature.velocity.getTheta() * 180 / math.pi,
+        #                                                       creature.velocity.magnitude()), False, (255, 255, 255))
+        # distance_text = font.render('Distance to target {:.02f}'.format(creature.position.distance(target.position)), False, (255, 255, 255))
+        # accell_text = font.render('Applied acceleration: ({:.02f},{:.02f})'.format(creature.prev_acc.x, creature.prev_acc.y), False, (255, 255, 255))
         #
-        #     for creature in environment["creatures"]:
-        #         if creature.alive:
-        #             creature.C_update(environment)
-        #             creature.draw();
-
-        target.move(pg.mouse.get_pos())
-        pg.draw.circle(canvas, (255, 255, 255), (target.position.x, target.position.y), 20, 1)
-        creature.test_seek(target)
-        creature.update()
-        creature.draw()
-        compass.show(creature.velocity.getTheta())
-        heading_text = font.render('Heading: {:.02f}  Velocity: {:.02f}'.format(creature.velocity.getTheta() * 180 / math.pi,
-                                                              creature.velocity.magnitude()), False, (255, 255, 255))
-        distance_text = font.render('Distance to target {:.02f}'.format(creature.position.distance(target.position)), False, (255, 255, 255))
-        accell_text = font.render('Applied acceleration: ({:.02f},{:.02f})'.format(creature.prev_acc.x, creature.prev_acc.y), False, (255, 255, 255))
-
-        canvas.blit(heading_text, (WIDTH - 500, HEIGHT - 200))
-        canvas.blit(distance_text, (WIDTH - 500, HEIGHT - 150))
-        canvas.blit(accell_text, (WIDTH - 500, HEIGHT - 100))
+        # canvas.blit(heading_text, (WIDTH - 500, HEIGHT - 200))
+        # canvas.blit(distance_text, (WIDTH - 500, HEIGHT - 150))
+        # canvas.blit(accell_text, (WIDTH - 500, HEIGHT - 100))
         pg.display.flip()
 
-        pg.time.delay(100)
+        # pg.time.delay(100)
 
