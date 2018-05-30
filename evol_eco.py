8b40b3f494ca7513e05905e28db78c3e191880ad
bfallin <fallinbryan@gmail.com>
2018-05-30 13:58:58 -0500

    dna paramters added creatures breed with opposite sex genes crossover Stable evolutionary ecosystem achieved

--- evol_eco.py
+++ evol_eco.py
@@ -47,20 +47,29 @@ class DNA(object):
         if genes:
             self.genes = genes
         else:
-            self.genes = [8]
+            self.genes = {}
+            self.genes['max_size'] = random.randrange(5, 30)
+            self.genes['growth_rate'] = random.uniform(0.001, 0.9)
+            self.genes['max_speed'] = random.randrange(5, 100)
+            self.genes['lifespan'] = random.randrange(100, 1000)
+            self.genes['breeding_age'] = random.randrange(50, self.genes['lifespan'])
+            self.genes['breeding_cost'] = random.randrange(100, 500)
+            self.genes['feeding_behaviour'] = random.choice(['near','far'])
 
     def crossover(self, partner):
-        newDna = []
-        pivot = random.randrange(0, len(self.genes))
-        for i, gene in  enumerate(self.genes):
-            if i > pivot:
-                newDna.append(gene)
-            else:
-                newDna.append(partner.genes[i])
-        return DNA(genes=newDna)
+        new_genes = {}
+        for name in self.genes:
+            new_genes[name] = random.choice([self.genes[name], partner.genes[name]])
+
+        return DNA(genes=new_genes)
 
     def mutation(self):
-        pass
+        for name in self.genes:
+            if name != 'feeding_behaviour':
+                if random.random() > .99:
+                    self.genes[name] += random.uniform(-0.01, 0.01)
+        if random.random() > .9999:
+            self.genes['feeding_behaviour'] = random.choice(['near', 'far'])
 
 
 class Creature(Physics):
@@ -76,13 +85,17 @@ class Creature(Physics):
         self.age = 1.0
         self.alive = True
         self.sex = random.choice([True, False])
-        self.max_size = 20
-        self.growth_rate = 1.0/30.0
-        self.max_speed = 10
+        self.max_size = dna.genes['max_size']
+        self.growth_rate = dna.genes['growth_rate']
+        self.max_speed = dna.genes['max_speed']
         self.prev_acc = Vector2D(0, 0)
         self.state = "wandering"
         self.target = None
         self.targetReached = True
+        self.lifespan = dna.genes['lifespan']
+        self.breeding_age = dna.genes['breeding_age']
+        self.breeding_cost = dna.genes['breeding_cost']
+        self.feeding_behaviour = dna.genes['feeding_behaviour']
 
     def apply_force(self, force):
         super().apply_force(force)
@@ -108,15 +121,34 @@ class Creature(Physics):
 
 
     def test_seek(self, target):
-        force = self.seek(target)
-        # force.set_magnitude(1)
-        self.apply_force(force)
-        mag = self.position.distance(target.position)*0.5
-        self.velocity.set_magnitude(mag)
+        self.target = target
+        self.seek()
 
-        if self.velocity.magnitude() > self.max_speed:
-            self.velocity.set_magnitude(self.max_speed)
 
+    def seek_furthest(self, targets):
+        x_max = self.surface.get_width()
+        y_max = self.surface.get_height()
+        max_dist = 0
+        target = targets[0]
+        for _target in targets:
+            if self.position.distance(_target.position) > max_dist:
+                if _target is not self:
+                    try:
+                        if _target.sex == self.sex ^ True and _target.state == "seeking mate":
+                            max_dist = self.position.distance(_target.position)
+                            target = _target
+
+                    except:
+                        max_dist = self.position.distance(_target.position)
+                        target = _target
+        self.target = target
+        self.seek()
+        found = None
+
+        if self.position.distance(self.target.position) < 20 and target is not self:
+            found = target
+
+        return found
     def seek_nearest(self, targets):
         x_max = self.surface.get_width()
         y_max = self.surface.get_height()
@@ -165,7 +197,7 @@ class Creature(Physics):
         return_objects = []
         if self.energy == 0:
             self.alive = False
-        if self.age > 1000.0:
+        if self.age > self.lifespan:
             self.alive = False
 
         if self.alive:
@@ -174,11 +206,14 @@ class Creature(Physics):
 
             if self.energy < 400 and len(environment['food']) > 0:
                 self.state = "seeking food"
-                found = self.seek_nearest(environment['food'])
+                if self.feeding_behaviour == 'near':
+                    found = self.seek_nearest(environment['food'])
+                elif self.feeding_behaviour == 'far':
+                    found = self.seek_furthest(environment['food'])
                 if found:
                     self.eat_food(found)
 
-            elif self.age > 100 and self.energy > 400:
+            elif self.age > self.breeding_age and self.energy > self.breeding_cost:
                 self.state = "seeking mate"
                 found = self.seek_nearest(environment['creatures'])
                 if found:
@@ -186,7 +221,7 @@ class Creature(Physics):
                         return_objects.append(self.breed(found.dna))
                         return_objects.append(self.breed(found.dna))
 
-                        self.alive = False
+                        self.energy -= self.breeding_cost
                 else:
                     pass
 
@@ -203,7 +238,10 @@ class Creature(Physics):
             color[2] = 200
         else:
             color[2] = 50
-        size = self.max_size * (math.exp(self.age*self.growth_rate) / (math.exp(self.age*self.growth_rate) + 1))
+        try:
+            size = self.max_size * (math.exp(self.age*self.growth_rate) / (math.exp(self.age*self.growth_rate) + 1))
+        except OverflowError:
+            size = self.max_size * (math.exp(700) / (math.exp(700) + 1))
         pointlist = []
         pointlist.append((int(self.position.x + size*math.cos(self.velocity.getTheta())), int(self.position.y + size*math.sin(self.velocity.getTheta()))))
         pointlist.append((int(self.position.x - (size-5)*math.cos(self.velocity.getTheta())), int(self.position.y - (size-5)*math.sin(self.velocity.getTheta()))))
@@ -239,8 +277,15 @@ if __name__ == '__main__':
     WIDTH = canvas.get_width()
     HEIGHT = canvas.get_height()
 
-    environment = {"food": [Food(random.randrange(0, WIDTH), random.randrange(0, HEIGHT)) for _ in range(10)],
-                   "creatures": [Creature(canvas, Vector2D(random.randrange(0, WIDTH), random.randrange(0, HEIGHT)), DNA()) for _ in range(2)]}
+    environment = {"food": [],
+                   "creatures": [Creature(canvas, Vector2D(random.randrange(0, WIDTH), random.randrange(0, HEIGHT)), DNA()) for _ in range(50)]}
+
+    theta = 0
+    food_field_radius = 400
+    for _ in range(100):
+        environment['food'].append(Food(disp_w/2 + food_field_radius * math.cos(theta), disp_h/2 + food_field_radius * math.sin(theta)))
+        theta += .13
+
 
     class Target:
         def __init__(self):
@@ -251,17 +296,26 @@ if __name__ == '__main__':
     creature = Creature(canvas, Vector2D(WIDTH/2, HEIGHT/2), DNA())
     compass = Compass(canvas, Vector2D(WIDTH*0.1, HEIGHT*0.1), 'Creature Heading')
     isGrabbed = False
+    frame_rate = 100
+    chase_mouse = False
     while True:
+        food_field_radius = random.choice([50,100,150,200,225,250,300])
+        # if food_field_radius < 1:
+        #     food_field_radius = 400
+        # theta += .13
+        # if theta > 500:
+        #     theta = 0
+        theta = random.uniform(0, 2*math.pi)
         # pg.time.wait(100)
         for event in pg.event.get():
             if event.type == pg.QUIT:
                 pg.quit()
                 sys.exit(1)
             if event.type == pg.MOUSEBUTTONDOWN:
-                isGrabbed = True
+                chase_mouse = True
 
             if event.type == pg.MOUSEBUTTONUP:
-                isGrabbed = False
+                chase_mouse = False
 
             if event.type == pg.MOUSEMOTION:
                 if isGrabbed:
@@ -272,6 +326,10 @@ if __name__ == '__main__':
                     pass
                 elif pg.key.get_pressed()[pg.K_SPACE]:
                     pass
+                elif pg.key.get_pressed()[pg.K_UP]:
+                    frame_rate -= 10
+                elif pg.key.get_pressed()[pg.K_DOWN]:
+                    frame_rate += 10
                 elif pg.key.get_pressed()[pg.K_ESCAPE]:
                     pg.quit()
                     sys.exit(1)
@@ -286,24 +344,30 @@ if __name__ == '__main__':
                 if not food.eaten:
                     pg.draw.circle(canvas, (50, 255, 50), food.position.floor()(), 2, 0)
 
-        for count, creature in enumerate(environment["creatures"]):
-            if count > 50:
-                creature.alive = False
-            if creature.alive:
-                new_creatures = creature.C_update(environment)
+        if not chase_mouse:
+            if len(environment['creatures']) == 0:
+                environment['creatures'] += [Creature(canvas, Vector2D(random.randrange(0, WIDTH), random.randrange(0, HEIGHT)), DNA()) for _ in
+                 range(10)]
+            for count, creature in enumerate(environment["creatures"]):
+                if count > 50:
+                    creature.alive = False
+                if creature.alive:
+                    new_creatures = creature.C_update(environment)
+                    creature.draw()
+                    if new_creatures:
+                            environment["creatures"] += new_creatures
+
+                else:
+                    # environment['food'].append(Food(creature.position.x, creature.position.y, 500))
+                    environment['food'].append(Food(disp_w/2 + food_field_radius * math.cos(theta), disp_h/2 + food_field_radius * math.sin(theta)))
+                    environment['creatures'].pop(environment['creatures'].index(creature))
+        else:
+            target.move(pg.mouse.get_pos())
+            pg.draw.circle(canvas, (255, 80, 80), (target.position.x, target.position.y), 10, 0)
+            for creature in environment['creatures']:
+                creature.test_seek(target)
+                creature.update()
                 creature.draw()
-                if new_creatures:
-                        environment["creatures"] += new_creatures
-
-            else:
-                environment['food'].append(Food(creature.position.x, creature.position.y, 500))
-                environment['creatures'].pop(environment['creatures'].index(creature))
-
-        # target.move(pg.mouse.get_pos())
-        # pg.draw.circle(canvas, (255, 255, 255), (target.position.x, target.position.y), 20, 1)
-        # creature.test_seek(target)
-        # creature.update()
-        # creature.draw()
         # compass.show(creature.velocity.getTheta())
         # heading_text = font.render('Heading: {:.02f}  Velocity: {:.02f}'.format(creature.velocity.getTheta() * 180 / math.pi,
         #                                                       creature.velocity.magnitude()), False, (255, 255, 255))
@@ -315,5 +379,5 @@ if __name__ == '__main__':
         # canvas.blit(accell_text, (WIDTH - 500, HEIGHT - 100))
         pg.display.flip()
 
-        pg.time.delay(50)
+        pg.time.delay(frame_rate)
 
